7c41a33ac77bbf62a12628b4696db99118e48627
Fedor Arbuzov <arbuzovfp@gmail.com>
2021-10-14 23:30:59 +0300

    add url

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+let express = require('express');
+let axios = require('axios');
+
 
 const url = 'https://api.telegram.org/bot';
 const apiToken = '2080851679:AAHe4ELNXqRxfymhzeSST5swFE7i5qYgjkE';
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/', (req, res) => {  
-    // console.log(req.body);
-    const chatId = req.body.message.chat.id;
-    const sentMessage = req.body.message.text;
-    // Regex for hello
-    if (sentMessage.match(/hello/gi)) {
-         axios.post(`${url}${apiToken}/sendMessage`,
-              {
-                   chat_id: chatId,
-                   text: 'hello back 👋'
-              })
-              .then((response) => { 
-                   res.status(200).send(response);
-              }).catch((error) => {
-                   res.send(error);
-              });
-    } else {
-         // if no hello present, just respond with 200 
-         res.status(200).send({});
-    }
+var app = express();
+
+app.use(express.json());
+
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+  axios.post(`${url}${apiToken}/sendMessage`,
+  {
+       chat_id: 340473490,
+       text: 'hello back 👋'
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .then((res) => { 
+      response.status(200).send(res);
+  }).catch((error) => {
+      response.send(error);
+  });
+});
+
+app.listen(5000);
 
 
 
