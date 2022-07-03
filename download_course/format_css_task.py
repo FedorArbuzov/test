528a5dff0c190a6a263df1464f0c09dc326e1a6d
Fedor Arbuzov <arbuzovfp@gmail.com>
2022-07-03 12:54:55 +0300

    fix

--- download_course/format_css_task.py
+++ download_course/format_css_task.py
@@ -1,8 +1,11 @@
 from os import listdir
 from os.path import isfile, join
 
+import requests
 from bs4 import BeautifulSoup
 
+
+url = f'https://schoolsw3.com/css/'
 
 html_template = """
 <html>
@@ -15,62 +18,46 @@ html_template = """
 </html>
 """
 
-mypath = 'css_task'
+mypath = 'css_formated'
 mypath_formated = 'css_task_formated'
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print(onlyfiles)
+
+def format_css_try(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    src_ya = soup.find('script', {"src": "https://www.googletagmanager.com/gtag/js?id=UA-106562886-1"})
+    src_ya.decompose()
+    for script_tag in soup.find_all('script'):
+        script_tag_contents = script_tag.get_text()
+        if "https://mc.yandex.ru/metrika/tag.js" in script_tag_contents:
+            script_tag.decompose()
+    return str(soup)    
 
 for i in onlyfiles:
     try:
+        print(i)
         f = open(f'{mypath}/{i}', encoding='utf-8')
         soup = BeautifulSoup(f.read(), 'html.parser')
         f.close()
+        for i in soup.find_all('a'):
+            href = i['href']
+            if 'trycss' in href:
+                print(href)
+                if 'filename' in href:
+                    result_link = href.split('filename=')[1]
+                else:
+                    result_link = href.split('.')[0]
+                    print(result_link)
+                    r = requests.get(url + href)
+                    with open(f'css_try/{result_link}.html', 'w', encoding='utf-8') as file:
+                        file.write(format_css_try(r.text))
+                    break
 
-        src_ya = soup.find('link', {"href": "/lib/codemirror.css"})
-        src_ya.decompose()
-        src_ya = soup.find('script', {"src": "/lib/codemirror.js"})
-        src_ya.decompose()
+            
 
-        for script_tag in soup.find_all('script'):
-            script_tag_contents = script_tag.get_text()
-            if "https://mc.yandex.ru/metrika/tag.js" in script_tag_contents:
-                script_tag.decompose()
-
-        codemirrorCSS = soup.new_tag("link", href="../codemirror_html.css", rel="stylesheet")
-        w3 = soup.new_tag("link", href="../w3.css", rel="stylesheet")
-        codemirrorJS = soup.new_tag("script", src="../codemirror_html.js")
-        codemirrorJSSetup = soup.new_tag("script", src="../codemirror_setup.js")
-
-        soup.html.head.append(codemirrorCSS)
-        soup.html.head.append(w3)
-        soup.html.head.append(codemirrorJS)
-        soup.html.body.append(codemirrorJSSetup)
-
-        src_ya = soup.find('script', {"src": "https://yandex.ru/ads/system/context.js"})
-        src_ya.decompose()
-        src_ya = soup.find('script', {"src": "https://www.googletagmanager.com/gtag/js?id=UA-106562886-1"})
-        src_ya.decompose()
-        menu = soup.find('div', {"id": "menubtn"})
-        menu['style'] = "display: none;"
-        menu_items = soup.find('div', {"id": "exercisemenu"})
-        menu_items['style'] = "display: none;"
-        div1 = soup.find('div', {"id": "id01"})
-        div1['style'] = "display: none;"
-        div2 = soup.find('div', {"id": "id02"})
-        div2['style'] = "display: none;"
-
-        div2 = soup.find('td', {"id": "codeTD"})
-        div2['style'] = "padding-left: 20px;"
-
-        div2 = soup.find('td', {"id": "topTD"})
-        div2['style'] = "padding-left: 20px;"
-
-        div2 = soup.find('td', {"id": "codeTD2"})
-        div2['style'] = "padding-left: 20px;"
-
-
-        f = open(f'pages/css_tasks/{i}', 'w', encoding='utf-8')
-        f.write(str(soup))
-        f.close()
+        # f = open(f'pages/css_theory/{i}', 'w', encoding='utf-8')
+        # f.write(str(soup))
+        # f.close()
     except Exception as e:
         print(e, i)
--- download_course/main.py
+++ download_course/main.py
@@ -4,22 +4,22 @@ from bs4 import BeautifulSoup
 
 theme = 'css_task'
 
-url = f'https://schoolsw3.com/css/exercise_selectors1.php'
+url = f'https://schoolsw3.com/css/trycss_border_round.php'
 
 r = requests.get(url)
 with open('index.html', 'w') as file:
     file.write(r.text)
 
-soup = BeautifulSoup(r.text, 'html.parser')
+# soup = BeautifulSoup(r.text, 'html.parser')
 
-header = soup.find('div', {"class": "exercisemenuinner"})
-res = header.select('a[class="exbtn"]')
+# header = soup.find('div', {"class": "exercisemenuinner"})
+# res = header.select('a[class="exbtn"]')
 
-url = f'https://schoolsw3.com/css/'
-for i in res:
-    link = i['href']
-    name = link.split('.')[0]
-    r = requests.get(url + link)
-    print(name)
-    with open(f'{theme}/{name}.html', 'w', encoding='utf-8') as file:
-        file.write(r.text)
+# url = f'https://schoolsw3.com/css/'
+# for i in res:
+#     link = i['href']
+#     name = link.split('.')[0]
+#     r = requests.get(url + link)
+#     print(name)
+#     with open(f'{theme}/{name}.html', 'w', encoding='utf-8') as file:
+#         file.write(r.text)
